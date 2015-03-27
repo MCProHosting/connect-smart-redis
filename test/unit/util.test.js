5cac60e53e6a716ab01f17eb352eebdbb13d2aeb
Connor Peet <connor@peet.io>
2015-03-27 12:13:59 -0400

    Fix several bugs/cases

--- test/unit/util.test.js
+++ test/unit/util.test.js
@@ -4,11 +4,11 @@ describe('clone', function () {
     var clone = require('../../lib/util').clone;
 
     it('works correctly', function () {
-        var a = { b: 'c' };
+        var a = { b: 'c', c: null };
         var b = clone(a);
         a.b = 'd';
 
         expect(a.b).to.equal('d');
-        expect(b.b).to.equal('c');
+        expect(b).to.deep.equal({ b: 'c', c: null });
     });
 });
