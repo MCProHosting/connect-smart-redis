6022ce7a4c1edc882f0bca496835cdb66b5b310e
Connor Peet <connor@peet.io>
2015-03-27 12:19:39 -0400

    Handle arrays correctly, too

--- test/unit/util.test.js
+++ test/unit/util.test.js
@@ -4,11 +4,12 @@ describe('clone', function () {
     var clone = require('../../lib/util').clone;
 
     it('works correctly', function () {
-        var a = { b: 'c', c: null };
+        var a = { b: 'c', c: null, d: [1, 2] };
         var b = clone(a);
         a.b = 'd';
+        a.d.push(3);
 
-        expect(a.b).to.equal('d');
-        expect(b).to.deep.equal({ b: 'c', c: null });
+        expect(a).to.deep.equal({ b: 'd', c: null, d: [1, 2, 3] });
+        expect(b).to.deep.equal({ b: 'c', c: null, d: [1, 2] });
     });
 });
